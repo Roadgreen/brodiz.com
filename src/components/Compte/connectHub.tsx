b34bdba4b9ef01231d1ead22f3f4b8b17b8c01b8
Roadgreen <roadgreenproduction@gmail.com>
2023-07-28 18:56:30 +0200

    new

--- src/components/Compte/connectHub.tsx
+++ src/components/Compte/connectHub.tsx
@@ -5,16 +5,32 @@ import styles from './connectHub.module.css'
 function ConnectHub() {
     const User = useContext(UserContext);
     console.log(User?.userFind);
+    const handleClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      const email = User.userEmail;
+      
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function isValidEmail(email:string) {
+  return emailRegex.test(email);
+}
+
+if(isValidEmail(email) === true){
+const finded = User?.FindUser({email,collection:'Client'});
+console.log('dans le hub reponse',finded)
+} else if(isValidEmail(email) === true && email === 'Admin@Admin.com'){
+  const finded = User?.FindUser({email,collection:'Admin'});
+console.log('dans le hub reponse',finded)
+}
+    }
+
   return (
     <div className={styles.Container}>
         <h1>Mon compte</h1>
   <div className={styles.formContainer}><form>
     <p>Indiquez votre email pour l'inscription ou vous connecter</p>
     <div className={styles.InputContainer}>
-      
-    <input  pattern="[a-zA-Z]+" required></input>
+    <input onChange={(e:any)=>{User?.setUserEmail(e.target.value)}} pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"  required></input>
     <span className={styles.spanFoc}>Email</span>
-
     </div>
     {User?.userFind === "Wait" ? <></>  : 
     <>
@@ -22,7 +38,6 @@ function ConnectHub() {
     <input pattern="[a-zA-Z]+" required ></input>
     <span className={styles.spanFoc}>Mdp</span>
     </div>
-   
     <p>S'inscrire à la newsletter : </p>
     <div className={styles.newsChecked}>
     <label>Oui</label>
@@ -32,10 +47,7 @@ function ConnectHub() {
     </div>
     </>
   }
-    <button className={styles.button}>continuer</button>
-    
-  
-
+    <button onClick={e=>handleClick(e)} className={styles.button}>continuer</button>
       </form></div>
 </div>
   )
--- src/components/Function/UserAccountContext.tsx
+++ src/components/Function/UserAccountContext.tsx
@@ -1,78 +1,114 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, Dispatch, SetStateAction } from "react";
 
 type UserContext = {
-CreateAccount:(User:User) => void,Login: (User:User) => void,FindUser: (User:UserSearch) => void,isConnected: any, isBuyers: any, isNews: any, userFind:any
-}
+  CreateAccount: (User: User) => void;
+  Login: (User: User) => void;
+  FindUser: (User: UserSearch) => void;
+  isConnected: any;
+  isBuyers: any;
+  isNews: any;
+  userFind: any;
+  userEmail: string;
+  setUserEmail: Dispatch<SetStateAction<string>>;
+};
 interface User {
-email: string,
-password: string,
-newsletter:boolean,
-date:string
+  email: string;
+  password: string;
+  newsletter: boolean;
+  date: string;
+  collection: string;
 }
 interface UserSearch {
-    email: string,
-    username: string,
-    }
+  email: string;
+  collection:string;
+}
 
+export const UserContext = createContext<UserContext>({} as UserContext);
 
-export const UserContext = createContext<UserContext | null>(null);
+export default function UserContextProvider({ children }: any) {
+  const [userFind, setUserFind] = useState("Wait");
+  const [isConnected, setIsConnected] = useState(false);
+  const [isBuyers, setIsBuyers] = useState(false);
+  const [isNews, setIsNews] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-export default function UserContextProvider({children}:any) {
-    const [userFind,setUserFind] = useState('Wait');
-    const [isConnected,setIsConnected] = useState(false);
-    const [isBuyers,setIsBuyers] = useState(false);
-    const [isNews, setIsNews] = useState(false);
+  const CreateAccount = async (User: User) => {
+    const { email, password, newsletter, date } = User;
 
-    const CreateAccount = async (User:User) => {
-const {
-    email,password,newsletter,date
-}  = User;
+    var myInit = {
+      method: "POST",
+      mode: "cors" as RequestMode,
+      body: JSON.stringify({
+        email,
+        password,
+        newsletter,
+        date,
+      }),
+    };
 
-var myInit = { method: 'POST',
-               mode: 'cors'as RequestMode,
-               body: JSON.stringify({
-                email,password
-                ,newsletter,date
-               })};
+    const response = await fetch(
+      process.env.FETCHLOGIN || "http://localhost:8080/users/addUser",
+      myInit
+    ).then((res) => {
+      console.log(res);
+    });
+  };
 
-const response = await  fetch(process.env.FETCHLOGIN || 'http://localhost:8080/users/addUser',myInit).then((res)=>{
-    console.log(res);
-})
-    }
+  const Login = async (User: User) => {
+    const { email, password } = User;
 
-    const Login = async (User:User) => {
-        const {
-            email,password
-        }  = User;
-        
-        var myInit = { method: 'POST',
-                       mode: 'cors'as RequestMode,
-                       body: JSON.stringify({
-                        email,password
-                       })};
-        
-        const response = await  fetch(process.env.FETCHLOGIN || 'http://localhost:8080/users/login',myInit).then((res)=>{
-            console.log(res);
-        })
-            }
+    var myInit = {
+      method: "POST",
+      mode: "cors" as RequestMode,
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    };
 
-            const FindUser = async (User:UserSearch) => {
-                const {
-                    email,username
-                }  = User;
-                
-                var myInit = { method: 'POST',
-                               mode: 'cors'as RequestMode,
-                               body: JSON.stringify({
-                                email,username
-                               })};
-                
-                const response = await  fetch(process.env.FETCHLOGIN || 'http://localhost:8080/users/findUser',myInit).then((res)=>{
-                    console.log(res);
-                })
-                    }    
+    const response = await fetch(
+      process.env.FETCHLOGIN || "http://localhost:8080/users/login",
+      myInit
+    ).then((res) => {
+      console.log(res);
+    });
+  };
 
-    return <UserContext.Provider value={{
-      CreateAccount,Login,FindUser,isConnected,isBuyers,isNews,userFind
-      }}>{children}</UserContext.Provider>;
-    }
+  const FindUser = async (User: UserSearch) => {
+    const { email,collection } = User;
+console.log(collection)
+    var myInit = {
+      method: "POST",
+      mode: "cors" as RequestMode,
+      body: JSON.stringify({
+        email,collection
+      }),
+    };
+
+    const response = await fetch(
+      process.env.FETCHLOGIN || "http://localhost:8080/users/findUser",
+      myInit
+    ).then((res) => {
+      console.log('reponse du context',res);
+
+    });
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        CreateAccount,
+        Login,
+        FindUser,
+        isConnected,
+        isBuyers,
+        isNews,
+        userFind,
+        userEmail,
+        setUserEmail,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}

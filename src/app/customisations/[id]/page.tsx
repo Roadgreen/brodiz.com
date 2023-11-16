09d5346cb840f3dc88a43b141326e34fdbb24708
Roadgreen <roadgreenproduction@gmail.com>
2023-11-16 19:06:01 +0100

    customisation product

---
+++ src/app/customisations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CustomHoodies from '@/components/customisationsPage/customisationsProduct/customHoodiesAnimals/customHoodies'
+
+export default function page() {
+  return (
+    <>
+    <CustomHoodies/>
+    </>
+  )
+}

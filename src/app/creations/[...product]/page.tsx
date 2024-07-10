8bd2fadca626412d43234922c1d509d8675664e7
Roadgreen <roadgreenproduction@gmail.com>
2024-07-10 12:55:46 +0200

    Problème lengt

--- src/app/creations/[...product]/page.tsx
+++ src/app/creations/[...product]/page.tsx
@@ -22,7 +22,7 @@ export default function ProductPage({
     useEffect(() => {
       console.log(selectedProduct, Object.keys(selectedProduct));
       async function searchProduct() {
-        if (selectedProduct.id.length === 0) {
+        if (!selectedProduct || !selectedProduct.id || selectedProduct.id.length === 0) {
           const product = await productSearch(
             { id: params.product[1] },
             params.product[0],

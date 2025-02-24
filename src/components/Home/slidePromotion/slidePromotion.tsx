36bf7c8076d11de6cd3b2f68ca35027dd0fb6c7b
Roadgreen <roadgreenproduction@gmail.com>
2025-02-24 20:55:53 +0100

    changement de bento

--- src/components/Home/slidePromotion/slidePromotion.tsx
+++ src/components/Home/slidePromotion/slidePromotion.tsx
@@ -7,6 +7,11 @@ import womenWhite from '@/productImg/Home/womensweat.jpg'
 import MenWhite from '@/productImg/Home/Mensweatchisrt.jpg'
 import { useRouter } from 'next/navigation'
 import babyjedi from '../../../productImg/Home/babyjedi.jpg'
+import serpent from '../../../productImg/Home/serpentard.jpg'
+import quarta from '../../../productImg/Home/quarte.jpg'
+import gryf from '../../../productImg/Home/gryf.jpg'
+import littlecat from '../../../productImg/Home/littlecat.jpg'
+import { color } from "html2canvas/dist/types/css/types/color";
 export default function SlidePromotion() {
   const router = useRouter()
   const para = useRef<any>();
@@ -18,28 +23,35 @@ export default function SlidePromotion() {
   return (
     <div   className={styles.container}>
  <div ref={para} className={styles.Bento1}>
+ <Link style={{ textDecoration: "none" }} href={'/creations'}>
  <div className={styles.littleBentoHor}>
-        <h3>Collection Homme</h3>
+        <h3 >Collection Homme</h3>
         <Image className={styles.Img} src={MenWhite} fill alt="Homme barbue avec sweatshirt blanc"/>
       </div>
+      </Link>
+      <Link style={{ textDecoration: "none" }} href={'/creations'}>
       <div className={styles.littleBentoHor}>
         <h3>Collection Femme</h3>
         <Image className={styles.Img} src={womenWhite} fill alt="deux femmes avec sweatshirt"/>
       </div>
-      <Link href={'/creations'}>
+      </Link>
+      <Link style={{ textDecoration: "none" }} href={'/creations/Baby/Body_Baby_FuturJedi_001s'}>
       <div className={styles.MiddleBentoHor}>
-        <h3>Collection Bébé</h3>
+        <h3 style={{ color: "black" }}>Collection Bébé</h3>
         <Image className={styles.Img} src={babyjedi} fill alt="Body enfant Jedi Star Wars"/>
       </div>
       </Link>
  </div>
+
  <div ref={para2} className={styles.Bento2}>
+ <Link style={{ textDecoration: "none" }} href={'/creations'}>
  <div className={styles.littleBentoHor}>
-        <h3>Collection Femme</h3>
-        <Image className={styles.Img} src={womenWhite} fill alt="deux femmes avec sweatshirt"/>
+        <h3>Collection enfant</h3>
+        <Image className={styles.Img} src={littlecat} fill alt="enfant avec sweat blanc et des petits chats"/>
       </div>
+      </Link>
  <div className={styles.BigBentoVer}>
-        <h3>Sweatshirt Bearded Man</h3>
+        <h3></h3>
         <Image className={styles.Img} src={MenWhite} fill alt="deux femmes avec sweatshirt"/>
       </div>
       <div className={styles.littleBentoHor}>
@@ -49,12 +61,12 @@ export default function SlidePromotion() {
  </div>
  <div ref={para3} className={styles.Bento3}>
  <div className={styles.littleBentoHor}>
-        <h3>Collection Homme</h3>
-        <Image className={styles.Img} src={MenWhite} fill alt="deux femmes avec sweatshirt"/>
+       <h3></h3>
+        <Image className={styles.Img} src={serpent} fill alt="Homme sweatshirt Harry potter"/>
       </div>
       <div className={styles.MiddleBentoHor}>
         <h3>Collection Homme</h3>
-        <Image className={styles.Img} src={MenWhite} fill alt="deux femmes avec sweatshirt"/>
+        <Image className={styles.Img} src={gryf} fill alt="deux femmes avec sweatshirt"/>
       </div>
     
       <div className={styles.littleBentoHor}>
@@ -63,7 +75,7 @@ export default function SlidePromotion() {
       </div>
       <div className={styles.MiddleBentoHor}>
         <h3>Collection Homme</h3>
-        <Image className={styles.Img} src={MenWhite} fill alt="deux femmes avec sweatshirt"/>
+        <Image className={styles.Img} src={quarta} fill alt="femmes sweat blanc Quartararo"/>
       </div>
 
  </div>
